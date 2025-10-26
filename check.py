bdc9bab5e036baeb23571afc3f61782a5170d1ed
ManasCodeCraft <manassrivastava567@gmail.com>
2025-10-26 10:24:30 +0530

    up

--- check.py
+++ check.py
@@ -1,24 +1,20 @@
 import requests, time
 
-# list of URLs to ping
 URLS = [
     "https://server.speedyutils.com",
     "https://utils.speedyutils.com",
 ]
 
-# run for 4 minutes → 4 requests (1/min)
-INTERVAL_SECONDS = 60
-TOTAL_MINUTES = 4
+INTERVAL_SECONDS = 60  # one request every minute
 
-def send_requests():
-    for minute in range(TOTAL_MINUTES):
+def loop_forever():
+    while True:
         for url in URLS:
             try:
-                # fire and forget (don't wait for response content)
                 requests.get(url, timeout=5)
             except Exception:
-                pass  # ignore all errors completely
+                pass  # ignore all errors
         time.sleep(INTERVAL_SECONDS)
 
 if __name__ == "__main__":
-    send_requests()
+    loop_forever()
